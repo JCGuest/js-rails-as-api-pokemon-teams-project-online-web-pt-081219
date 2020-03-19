1365fadfe25cfb0596428dc0b23bd6d9f4022d62
JCGuest <jcguest90@gmail.com>
2020-03-19 02:27:06 -0500

    rendered new card div for each trainer

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+fetch(TRAINERS_URL)
+.then(function(response) {
+    return response.json();
+})
+.then(function(json) { 
+    for (key in json) {
+        json[key].forEach(element => {
+            createCard(element)
+        })
+    }
+
+})
+
+function createCard(trainer) {
+    console.log(trainer['attributes']['name'])
+    let newDiv = document.createElement('div')
+    let trainerName = document.createElement('h1')
+    let main = document.querySelector("body > main")
+
+    newDiv.classList.add('card')
+    // trainer name
+    trainerName.innerHTML = `${trainer['attributes']['name']}`
+    newDiv.appendChild(trainerName)
+    // add poke button
+    main.appendChild(newDiv)
+}
+
+
+
+})
